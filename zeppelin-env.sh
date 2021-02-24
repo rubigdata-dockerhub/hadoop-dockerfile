cbdabc438fc78788690c9a177e2b5b9c3932c93e
Arjen P. de Vries <arjen@acm.org>
2021-02-24 22:31:02 +0100

    Update Zeppelin config to new Spark/Hadoop versions

--- zeppelin-env.sh
+++ zeppelin-env.sh
@@ -1,6 +1,6 @@
 # set Spark interpreter
-export SPARK_HOME=/spark-2.4.4
+export SPARK_HOME=/spark-3.0.2
 
 # set Hadoop config
-export HADOOP_CONF_DIR=/hadoop-3.2.1
+export HADOOP_CONF_DIR=/hadoop-3.2.2
 
