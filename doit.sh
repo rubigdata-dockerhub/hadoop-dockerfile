f9e6f72b562b6eb5b84123a9750622fb17a0022d
Arjen P. de Vries <arjen@acm.org>
2021-05-12 03:22:54 +0200

    Ok, needed the archive after all

--- doit.sh
+++ doit.sh
@@ -4,4 +4,5 @@ docker build -t "rubigdata/sshd"   --format docker --no-cache -f df-sshd .
 docker build -t "rubigdata/hadoop" --format docker --no-cache -f df-hadoop .
 docker build -t "rubigdata/spark"  --format docker --no-cache -f df-spark .
 docker build -t "rubigdata/spark-slim"  --format docker --no-cache -f df-spark-slim .
+tar czvfp rubigdata.tgz rubigdata
 docker build -t "rubigdata/project"  --format docker --no-cache -f df-project .
