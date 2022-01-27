4dd28ee204f72b024e610632b8c616d33eaa7f9b
Gijs Hendriksen <info@gijshendriksen.nl>
2022-01-27 17:11:09 +0100

    Update container configuration for 2021/2022

--- doit.sh
+++ doit.sh
@@ -1,3 +1,5 @@
+set -e
+
 docker build -t "rubigdata/base"   --format docker --no-cache -f df-base .
 docker build -t "rubigdata/tini"   --format docker --no-cache -f df-tini .
 docker build -t "rubigdata/sshd"   --format docker --no-cache -f df-sshd .
@@ -5,4 +7,5 @@ docker build -t "rubigdata/hadoop" --format docker --no-cache -f df-hadoop .
 docker build -t "rubigdata/spark"  --format docker --no-cache -f df-spark .
 docker build -t "rubigdata/spark-slim"  --format docker --no-cache -f df-spark-slim .
 tar czvfp rubigdata.tgz rubigdata
-docker build -t "rubigdata/project"  --format docker --no-cache -f df-project .
+docker build -t "rubigdata/course"  --format docker --no-cache -f df-course .
+docker build -t "rubigdata/redbad"  --format docker --no-cache -f df-redbad .
